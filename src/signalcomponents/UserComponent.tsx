a9059983c4f68eaf2e9b23526f9e7a27a30d8a4f
Johan Selin <johanselin99@gmail.com>
2023-02-14 10:48:46 +0100

    updated to comply with App.tsx

--- src/signalcomponents/UserComponent.tsx
+++ src/signalcomponents/UserComponent.tsx
@@ -5,14 +5,21 @@ import { HubConnection } from '@microsoft/signalr';
 
 export default function UserComponent(props: {connection: HubConnection}) {
     const [pageViewCount, setPageViewCount] = useState<number>(0)
+    const [connectedUsersCount, setConnectedUsersCount] = useState<number>(0)
 
     // connect to methods that hub invokes aka receive notifications from hub
     props.connection.on('updateTotalViews', (value) => {
         setPageViewCount(value)
     })
 
+    // When we get 'updateConnectedUsers' event from server we update the view
+    props.connection.on('updateConnectedUsers', (value) => {
+        setConnectedUsersCount(value)
+    }) 
+
     return (
-        <div>
+        <div style={{display: 'grid'}}>
+            <span>Users connected: {connectedUsersCount}</span>
             <span>Total Views: {pageViewCount}</span>
         </div>
     );
